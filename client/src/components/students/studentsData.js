275d043155883a425c7a24c83eccfd5ef15d2dae
oyesaurav <78659500+oyesaurav@users.noreply.github.com>
2021-09-13 00:26:11 +0530

    student page

---
+++ client/src/components/students/studentsData.js
@@ -0,0 +1,70 @@
+
+const StudentDetails = [
+    {
+        id: "B120001",
+        Name: "Abhishek Kumar",
+        DOB : "x / x / x",
+        Branch:"CSE",
+        Interest: "something",
+        Email: "B120001@iiit-bh.ac.in"
+    },
+    {
+        id: "B120002",
+        Name: "Adarsh Barik",
+        DOB : "x / x / x",
+        Branch: "CSE",
+        Interest: "something",
+        Email: "B120002@iiit-bh.ac.in"
+    },
+    {
+        id: "B120003",
+        Name: "Aditi Rout",
+        DOB : "x / x / x",
+        Branch: "CSE",
+        Interest: "something",
+        Email: "B120003@iiit-bh.ac.in"
+    },
+    {
+        id: "B120004",
+        Name: "Amisha",
+        DOB : "x / x / x",
+        Branch: "CSE",
+        Interest: "something",
+        Email: "B120004@iiit-bh.ac.in"
+    },
+    {
+        id: "B120005",
+        Name: "Abhishek Kumar",
+        DOB : "x / x / x",
+        Branch: "CSE",
+        Interest: "something",
+        Email: "B120001@iiit-bh.ac.in"
+    },
+    {
+        id: "B120006",
+        Name: "Abhishek Kumar",
+        DOB : "x / x / x",
+        Branch:"CSE",
+        Interest: "something",
+        Email: "B120001@iiit-bh.ac.in"
+    },
+    {
+        id: "B120007",
+        Name: "Abhishek Kumar",
+        DOB : "x / x / x",
+        Branch:"CSE",
+        Interest: "something",
+        Email: "B120001@iiit-bh.ac.in"
+    },
+    {
+        id: "B120008",
+        Name: "Abhishek Kumar",
+        DOB : "x / x / x",
+        Branch:"CSE",
+        Interest: "something",
+        Email: "B120001@iiit-bh.ac.in"
+    }
+
+]
+
+export default StudentDetails
