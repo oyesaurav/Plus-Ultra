4024d68669820ce7c9ba981716a513357e3cfe83
oyesaurav <78659500+oyesaurav@users.noreply.github.com>
2021-09-13 17:52:19 +0530

    Day 2 - Final

--- client/src/app.js
+++ client/src/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import Home from './components/homepage/Home';
 import Nav from './components/homepage/navbar';
-import Nav from './components/homepage/navbar';
+// import Nav from './components/homepage/navbar';
 
 export default function App() {
     return (
