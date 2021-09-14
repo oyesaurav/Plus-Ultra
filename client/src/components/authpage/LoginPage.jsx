8880098c33acb31457b65607774193c6b8899bee
SilverGraph <rj.rajeryan0071@gmail.com>
2021-09-14 18:43:11 +0530

    Login/Sign Up completed

--- client/src/components/authpage/LoginPage.jsx
+++ client/src/components/authpage/LoginPage.jsx
@@ -1,22 +1,29 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Footer from '../Footer'
 
 export default function LoginPage () {
 
     return(
-    <div className="login">
-         <p className="sign" align="center">Log in</p>
-        <form className="form1">
-           <input className="un " type="text" placeholder="Username" />
-           <input className="pass" type="password" placeholder="Password" />
-           <button type="submit" className="submit" align="center">Log in</button>
+        <div>
+            <div className="login">
 
-           <p className="forgot" align="center"><a href="#">Forgot Password?</a></p>
-           <p align="center">
-               <Link to="signup">Make an account first?</Link>
-            </p>
-        </form>  
+                <p className="sign" align="center">Log in</p>
+                <form className="form1">
+                <input className="un " type="text" placeholder="Username" />
+                <input className="pass" type="password" placeholder="Password" />
+                <button type="submit" className="submit" align="center">Log in</button>
+                <div>
+                    {/* eslint-disable-next-line */}
+                    <p id="forgot" align="center"><a href="#">Forgot Password?</a></p>
+                    <p id="make-account" align="center">
+                    <Link to="signup">Make an account first?</Link>
+                    </p>
+                </div>
+                </form>  
                 
-    </div>
+            </div>
+            <div  className="container-home"><Footer /></div>
+        </div>
     )
 }
